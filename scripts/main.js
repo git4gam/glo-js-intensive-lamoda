b1a19408041b77412eb34978fa62a1c72316ffc5
Andrey <girichev.andrey@gmail.com>
2021-07-06 14:11:21 +0300

    Day1: Modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const headerCityButton = document.querySelector('button.header__city-button');
+const headerCityButton = document.querySelector('.header__city-button');
 
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 
@@ -11,4 +11,20 @@ headerCityButton.addEventListener('click', () => {
 });
 
 
+// модальное окно
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
 
+const cartModalOpen = () => cartOverlay.classList.add('cart-overlay-open');
+const cartModalClose = () => cartOverlay.classList.remove('cart-overlay-open');
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+    console.dir(target.classList);
+    //if (target.classList.contains('cart__btn-close') || target.classList.contains('cart-overlay')) {
+    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cartModalClose();
+    }
+});
