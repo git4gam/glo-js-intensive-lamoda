ed213e0b8456e77eef8d1b8af748b412b82a46c7
Andrey Girichev <girichev.andrey@gmail.com>
2021-07-06 19:43:46 +0300

    Day1: Scroll lock

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,46 @@ headerCityButton.addEventListener('click', () => {
     localStorage.setItem('lomoda-location',city);    
 });
 
+// Блокировка скролла
+const disableScroll = () => {
+    //document.body.style.overflow = 'hidden';
+    document.body.dbScrollY = window.scrollY;
+    const scrollWidth = window.innerWidth - document.body.offsetWidth;
+    document.body.style.cssText = `
+        overflow: hidden;
+        padding-right: ${scrollWidth}px; 
+        /* cross-browser support*/
+        position: fixed;
+        width: 100%;
+        height: 100vh;
+        left: 0;
+        top: ${-window.scrollY}px;
+    `;
+    
+};
 
-// модальное окно
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll(
+        {
+            top: document.body.dbScrollY
+        }
+    );
+};
+
+// Модальное окно
 const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
 
-const cartModalOpen = () => cartOverlay.classList.add('cart-overlay-open');
-const cartModalClose = () => cartOverlay.classList.remove('cart-overlay-open');
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
+}  
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+    enableScroll();
+}
 
 subheaderCart.addEventListener('click', cartModalOpen);
 
@@ -25,6 +58,13 @@ cartOverlay.addEventListener('click', event => {
     console.dir(target.classList);
     //if (target.classList.contains('cart__btn-close') || target.classList.contains('cart-overlay')) {
     if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cartModalClose();
+    }
+});
+
+// Закрытие по клавише Escape
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
         cartModalClose();
     }
 });
