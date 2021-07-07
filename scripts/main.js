c57f53a87327c97dfac48454b9c298f0ecc7a327
Andrey <girichev.andrey@gmail.com>
2021-07-07 08:41:56 +0300

    Day2: Getting data

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,7 @@
 'use strict';
 
 const headerCityButton = document.querySelector('.header__city-button');
-
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
-
-headerCityButton.addEventListener('click', () => {
-    const city = prompt('Укажите ваш город').trim() || 'Ваш город?';
-    headerCityButton.textContent = city;
-    localStorage.setItem('lomoda-location',city);    
-});
 
 // Блокировка скролла
 const disableScroll = () => {
@@ -25,7 +18,6 @@ const disableScroll = () => {
         left: 0;
         top: ${-window.scrollY}px;
     `;
-    
 };
 
 const enableScroll = () => {
@@ -51,20 +43,51 @@ const cartModalClose = () => {
     enableScroll();
 }
 
+// Запрос базы данных
+const getData = async (source) => {
+    const data = await fetch(source);
+    if (data.ok) {
+        return data.json();
+    } else {
+        throw new Error(`Данные не были получены, ошибка ${data.status} ${data.statusText}`);
+    }
+};
+
+const getGoods = (callback) => {
+    getData('db.json')
+        .then(data => {
+            callback(data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+};
+
+// Event Listeners
+headerCityButton.addEventListener('click', () => {
+    const city = prompt('Укажите ваш город').trim() || 'Ваш город?';
+    headerCityButton.textContent = city;
+    localStorage.setItem('lomoda-location',city);    
+});
+
 subheaderCart.addEventListener('click', cartModalOpen);
 
 cartOverlay.addEventListener('click', event => {
     const target = event.target;
-    console.dir(target.classList);
+    //console.dir(target.classList);
     //if (target.classList.contains('cart__btn-close') || target.classList.contains('cart-overlay')) {
     if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
         cartModalClose();
     }
 });
 
-// Закрытие по клавише Escape
 document.addEventListener('keydown', event => {
     if (event.code === 'Escape') {
         cartModalClose();
     }
+});
+
+// Вызовы
+getGoods((data) => {
+    console.warn(data);
 });
