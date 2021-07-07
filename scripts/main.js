c4c6b4bd8f62788557f9252339fd32a2a35a09b6
Andrey <girichev.andrey@gmail.com>
2021-07-07 14:12:22 +0300

    Day2: Rendering goods p.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const headerCityButton = document.querySelector('.header__city-button');
-headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
+headerCityButton.textContent =
+  localStorage.getItem('lomoda-location') || 'Ваш город?';
 
 // Блокировка скролла
 const disableScroll = () => {
-    //document.body.style.overflow = 'hidden';
-    document.body.dbScrollY = window.scrollY;
-    const scrollWidth = window.innerWidth - document.body.offsetWidth;
-    document.body.style.cssText = `
+  //document.body.style.overflow = 'hidden';
+  document.body.dbScrollY = window.scrollY;
+  const scrollWidth = window.innerWidth - document.body.offsetWidth;
+  document.body.style.cssText = `
         overflow: hidden;
         padding-right: ${scrollWidth}px; 
         /* cross-browser support*/
@@ -21,12 +22,10 @@ const disableScroll = () => {
 };
 
 const enableScroll = () => {
-    document.body.style.cssText = '';
-    window.scroll(
-        {
-            top: document.body.dbScrollY
-        }
-    );
+  document.body.style.cssText = '';
+  window.scroll({
+    top: document.body.dbScrollY,
+  });
 };
 
 // Модальное окно
@@ -34,60 +33,100 @@ const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
 
 const cartModalOpen = () => {
-    cartOverlay.classList.add('cart-overlay-open');
-    disableScroll();
-}  
+  cartOverlay.classList.add('cart-overlay-open');
+  disableScroll();
+};
 
 const cartModalClose = () => {
-    cartOverlay.classList.remove('cart-overlay-open');
-    enableScroll();
-}
+  cartOverlay.classList.remove('cart-overlay-open');
+  enableScroll();
+};
 
 // Запрос базы данных
 const getData = async (source) => {
-    const data = await fetch(source);
-    if (data.ok) {
-        return data.json();
-    } else {
-        throw new Error(`Данные не были получены, ошибка ${data.status} ${data.statusText}`);
-    }
+  const data = await fetch(source);
+  if (data.ok) {
+    return data.json();
+  } else {
+    throw new Error(
+      `Данные не были получены, ошибка ${data.status} ${data.statusText}`
+    );
+  }
 };
 
 const getGoods = (callback) => {
-    getData('db.json')
-        .then(data => {
-            callback(data);
-        })
-        .catch(err => {
-            console.error(err);
-        });
+  getData('db.json')
+    .then((data) => {
+      callback(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 // Event Listeners
 headerCityButton.addEventListener('click', () => {
-    const city = prompt('Укажите ваш город').trim() || 'Ваш город?';
-    headerCityButton.textContent = city;
-    localStorage.setItem('lomoda-location',city);    
+  const city = prompt('Укажите ваш город').trim() || 'Ваш город?';
+  headerCityButton.textContent = city;
+  localStorage.setItem('lomoda-location', city);
 });
 
 subheaderCart.addEventListener('click', cartModalOpen);
 
-cartOverlay.addEventListener('click', event => {
-    const target = event.target;
-    //console.dir(target.classList);
-    //if (target.classList.contains('cart__btn-close') || target.classList.contains('cart-overlay')) {
-    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
-        cartModalClose();
-    }
+cartOverlay.addEventListener('click', (event) => {
+  const target = event.target;
+  //console.dir(target.classList);
+  //if (target.classList.contains('cart__btn-close') || target.classList.contains('cart-overlay')) {
+  if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+    cartModalClose();
+  }
 });
 
-document.addEventListener('keydown', event => {
-    if (event.code === 'Escape') {
-        cartModalClose();
-    }
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Escape') {
+    cartModalClose();
+  }
 });
+
+//getGoods((data) => {
+//  console.warn(data);
+//});
 
 // Вызовы
-getGoods((data) => {
-    console.warn(data);
-});
+try {
+  const goodsList = document.querySelector('.goods__list');
+  if (!goodsList) {
+    throw 'This is not a goods page!';
+  }
+
+  const createCard = (data) => {
+    const li = document.createElement('li');
+    li.classList.add('goods__item');
+    li.innerHTML = `
+            <article class="good">
+                <a class="good__link-img" href="card-good.html#${data.id}">
+                    <img class="good__img" src="goods-image/${data.preview}" alt="${data.name}">
+                 </a>
+                 <div class="good__description">
+                    <p class="good__price">${data.cost} &#8381;</p>
+                    <h3 class="good__title">${data.brand} <span class="good__title__grey">/ ${data.name} </span></h3>
+                    <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${data.sizes}</span></p>
+                    <a class="good__link" href="card-good.html#${data.id}">Подробнее</a>
+                 </div>
+            </article>
+        `;
+
+    return li;
+  };
+
+  const renderGoodsList = (data) => {
+    goodsList.textContent = '';
+    data.map((item) => {
+      goodsList.append(createCard(item));
+    });
+  };
+
+  getGoods(renderGoodsList);
+} catch (err) {
+  console.log(err);
+}
